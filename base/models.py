6348ffa5f51c8704201d37fed070b8778cb5c66d
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-04-18 17:10:35 +0430

    Users Cache

--- base/models.py
+++ base/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.timezone import now
 from django.db.models.signals import post_save
 
-from users.models import User
 from .signals import update_cache
 
 
@@ -27,7 +26,7 @@ post_save.connect(update_cache, sender=Sms)
 
 class Comment(Base):
     comment_related_parent = models.ForeignKey(Base, on_delete=models.CASCADE, related_name="comment_related_parent_name")
-    comment_related_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_related_user_name')
+    comment_related_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='comment_related_user_name')
     text = models.TextField()
 
 post_save.connect(update_cache, sender=Comment)
@@ -35,7 +34,7 @@ post_save.connect(update_cache, sender=Comment)
 
 class Rate(Base):
     rate_related_parent = models.ForeignKey(Base, on_delete=models.CASCADE, related_name='rate_related_parent_name')
-    rate_related_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rate_related_user_name')
+    rate_related_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='rate_related_user_name')
     title = models.CharField(max_length=50, blank=True, null=True)
     value = models.DecimalField(max_digits=6, decimal_places=2)
 
@@ -44,14 +43,14 @@ post_save.connect(update_cache, sender=Rate)
 
 class Favorite(Base):
     favorite_related_parent = models.ForeignKey(Base, on_delete=models.CASCADE, related_name='favorite_related_parent_name')
-    favorite_related_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorite_related_user_name')
+    favorite_related_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='favorite_related_user_name')
 
 post_save.connect(update_cache, sender=Favorite)
 
 
 class Discount(Base):
     discount_related_parent = models.ForeignKey(Base, on_delete=models.CASCADE, related_name='discount_related_parent_name')
-    discount_related_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='discount_related_user_name')
+    discount_related_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='discount_related_user_name')
     discount_value = models.IntegerField()
 
 post_save.connect(update_cache, sender=Discount)
@@ -59,7 +58,7 @@ post_save.connect(update_cache, sender=Discount)
 
 class Report(Base):
     report_related_parent = models.ForeignKey(Base, on_delete=models.CASCADE, related_name='report_related_parent_name')
-    report_related_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='report_related_user_name')
+    report_related_user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='report_related_user_name')
     report_text = models.TextField(blank=True, null=True)
 
 post_save.connect(update_cache, sender=Report)
--- users/models.py
+++ users/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
+from base.models import Base
 from base.signals import update_cache
 
-class User(AbstractUser):
+class User(AbstractUser, Base):
     GENDER_CHOICES = (
       ('female', 'Female'),
       ('male', 'Male')
     )
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, default='male')
 
-# post_save.connect(update_cache, sender=User)
+post_save.connect(update_cache, sender=User)
