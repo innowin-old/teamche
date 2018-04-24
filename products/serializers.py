70486a57c0afc3eeb478bda99da15c77080bc2be
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-04-24 16:22:43 +0430

    Read only fields in products serializers

--- products/serializers.py
+++ products/serializers.py
@@ -13,21 +13,33 @@ class ProductCategorySerializer(BaseSerializer):
     class Meta:
         model = ProductCategory
         fields = '__all__'
+        extra_kwargs = {
+          'product_category_related_user': { 'read_only': True }
+        }
 
 
 class ProductBrandSerializer(BaseSerializer):
     class Meta:
         model = ProductBrand
         fields = '__all__'
+        extra_kwargs = {
+          'product_brand_related_user': { 'read_only': True }
+        }
 
 
 class ProductSerializer(BaseSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+        extra_kwargs = {
+          'product_related_user': { 'read_only': True }
+        }
 
 
 class ProductPriceSerializer(BaseSerializer):
     class Meta:
         model = ProductPrice
         fields = '__all__'
+        extra_kwargs = {
+          'product_price_related_user': { 'read_only': True }
+        }
