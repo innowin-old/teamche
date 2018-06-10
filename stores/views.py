ea69e2a09a9386d468e9732f57d4b17c48a79484
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-06-10 14:06:18 +0430

    Fix stores vies

--- stores/views.py
+++ stores/views.py
@@ -17,6 +17,7 @@ from .models import (
 from .serializers import (
     StoreCategorySerializer,
     StoreSerializer,
+    StoreAdminSerializer,
     StoreDetailSerializer,
     StoreVisitSerializer,
     StoreVisitDetailSerializer
